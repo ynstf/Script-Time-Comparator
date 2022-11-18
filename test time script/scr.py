23b205c23b26f041b82643394a7d693a5f07938b
YONES <107154559+ynstf@users.noreply.github.com>
2022-11-18 22:54:42 +0100

    Delete test time script directory

--- test time script/scr.py
+++
@@ -1,3 +0,0 @@
-for i in range(1000000):
-    f=i**3
-    c=f-i
--- test time script/test.py
+++
@@ -1,8 +0,0 @@
-import time
-st= time.time()
-for i in range(1000000):
-    f=i**3
-    c=f-i
-tt = time.time() - st
-print('time of execution of this code is : ', tt)
-vr= input('press enter to leave')
