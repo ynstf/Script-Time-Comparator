11fd7954962032ff908ad1c36672d88bace50b46
YONES <107154559+ynstf@users.noreply.github.com>
2022-11-18 23:25:03 +0100

    Update test1.py

--- log/test1.py
+++ log/test1.py
@@ -1,9 +1,10 @@
 import os
 import time
 st= time.time()
-for i in range(10000):
-    f=i**3
-    c=f-i
+i = 0
+while(i<1000000):
+    i+=1
+    x=i**2
 tt = time.time() - st
 print('time of execution of this code is : ', tt)
 with open('log/values.txt', 'w') as val:
